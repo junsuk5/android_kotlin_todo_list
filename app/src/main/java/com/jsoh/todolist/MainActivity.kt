72933ba3b89cf653a9eb7e0b119ed562a267f9de
Junsuk Oh <a811219@gmail.com>
2020-03-16 15:55:45 +0900

    RecyclerView 어댑터의 뼈대 작성

--- app/src/main/java/com/jsoh/todolist/MainActivity.kt
+++ app/src/main/java/com/jsoh/todolist/MainActivity.kt
@@ -2,6 +2,11 @@ package com.jsoh.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,3 +17,24 @@ class MainActivity : AppCompatActivity() {
 }
 
 data class Todo(val text: String, var isDone: Boolean)
+
+class TodoAdapter(private val myDataset: List<Todo>) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): TodoAdapter.TodoViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_todo, parent, false)
+        return TodoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val textView = holder.view.findViewById<TextView>(R.id.todo_text)
+        textView.text = myDataset[position].text
+    }
+
+    override fun getItemCount() = myDataset.size
+}
