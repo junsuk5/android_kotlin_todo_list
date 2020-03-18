de71e8e2abdf66b837ecb060c0670d878ef62ab7
Junsuk Oh <a811219@gmail.com>
2020-03-18 14:32:23 +0900

    로그아웃 기능 작성

--- app/src/main/java/com/jsoh/todolist/MainActivity.kt
+++ app/src/main/java/com/jsoh/todolist/MainActivity.kt
@@ -5,8 +5,7 @@ import android.content.Intent
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
@@ -35,14 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         // 로그인이 안 됨
         if (FirebaseAuth.getInstance().currentUser == null) {
-            val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
-
-            startActivityForResult(
-                AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setAvailableProviders(providers)
-                    .build(),
-                RC_SIGN_IN)
+            login()
         }
 
 
@@ -84,6 +76,42 @@ class MainActivity : AppCompatActivity() {
                 // 로그인 실패
                 finish()
             }
+        }
+    }
+
+    fun login() {
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    fun logout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                login()
+            }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.action_log_out -> {
+                logout()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
