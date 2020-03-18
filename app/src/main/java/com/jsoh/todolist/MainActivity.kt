9efb408bdb9344077d55b2ada55980ef97389a2c
Junsuk Oh <a811219@gmail.com>
2020-03-18 21:02:17 +0900

    Firestore에 데이터 쓰기

--- app/src/main/java/com/jsoh/todolist/MainActivity.kt
+++ app/src/main/java/com/jsoh/todolist/MainActivity.kt
@@ -214,8 +214,9 @@ class MainViewModel: ViewModel() {
     }
 
     fun addTodo(todo: Todo) {
-        data.add(todo)
-        todoLiveData.value = data
+        FirebaseAuth.getInstance().currentUser?.let { user ->
+            db.collection(user.uid).add(todo)
+        }
     }
 
     fun deleteTodo(todo: Todo) {
